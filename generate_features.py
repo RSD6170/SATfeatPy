adf59466f932c37133e7aa5827bde9c878e1190e
RSD6170 <rsd6170+github@mailbox.org>
2024-12-12 09:23:14 +0000

    feat: add handling for subgroup analysis

--- generate_features.py
+++ generate_features.py
@@ -1,36 +1,77 @@
+import argparse
 import sys
+import json
+import tempfile
+import time
+from nis import match
+
 from sat_instance.sat_instance import SATInstance
+
+def doBasic(instance):
+    before = time.time()
+    instance.gen_basic_features()
+    after = time.time()
+    instance.features_dict["base_t"] = after - before
+
+def doDPLL(instance):
+    before = time.time()
+    instance.gen_dpll_probing_features()
+    after = time.time()
+    instance.features_dict["dpll_t"] = after - before
+
+def doLocalProbing(instance):
+    before = time.time()
+    instance.gen_local_search_probing_features()
+    after = time.time()
+    instance.features_dict["local_probing_t"] = after - before
+
+def doAnsotegui(instance):
+    before = time.time()
+    instance.gen_ansotegui_features()
+    after = time.time()
+    instance.features_dict["ansotegui_t"] = after - before
+
+def doMAG(instance):
+    before = time.time()
+    instance.gen_manthey_alfonso_graph_features()
+    after = time.time()
+    instance.features_dict["mag_t"] = after - before
+
 
 if __name__ == "__main__":
     # Ideal usage - call features, with filename to calculate from, and then options on preprocessing,
     # linux test setup
-    try:
-        cnf_path = sys.argv[1]
 
-    except IndexError as E:
-        print("no cnf path specified, using basic example")
-        cnf_path = "../or-60-20-5-UC-20.cnf.gz.no_w.cnf"
+    parser = argparse.ArgumentParser(prog="SATfeatPy")
+    parser.add_argument("--tmp_path", type=str, default=tempfile.gettempdir(), help="Path for temporary files")
+    parser.add_argument("--subset", help="Subset of features to analyze, defaults to all", choices=["all", "base", "dpll", "local", "ansotegui", "mag"], default="all")
+    parser.add_argument("cnf_path", help="Path to CNF to analyse in dimacs format")
 
-    if sys.platform == "darwin":
-        # osx
-        cnf_path = "cnf_examples/sat_4color_200_1126_10020_preprocessed.cnf"
-        cnf_path = "cnf_examples/count_10_4.cnf"
-        satinstance = SATInstance(cnf_path, preprocess=False)
+    args = parser.parse_args()
 
+    satinstance = SATInstance(args.cnf_path, preprocess=True, preprocess_tmp=True, tmpPath=args.tmp_path)
+
+    subset = args.subset
+
+    if subset == "all":
+        doBasic(satinstance)
+        doDPLL(satinstance)
+        # N.b. ubcsat binary currently only runs on linux
+        doLocalProbing(satinstance)
+        doAnsotegui(satinstance)
+        doMAG(satinstance)
+    elif subset ==  "base":
+        doBasic(satinstance)
+    elif subset ==  "dpll":
+        doDPLL(satinstance)
+    elif subset ==  "local":
+        doLocalProbing(satinstance)
+    elif subset ==  "ansotegui":
+        doAnsotegui(satinstance)
+    elif subset ==  "mag":
+        doMAG(satinstance)
     else:
-        satinstance = SATInstance(cnf_path, preprocess=True)
-
-    satinstance.gen_basic_features()
-
-    satinstance.gen_dpll_probing_features()
-
-    # N.b. ubcsat binary currently only runs on linux
-    satinstance.gen_local_search_probing_features()
-
-    satinstance.gen_ansotegui_features()
-
-    satinstance.gen_manthey_alfonso_graph_features()
-
-    print(satinstance.features_dict)
-
-    # satinstance.write_results()
+        print(f"Error, unknown argument for subset: {subset}")
+        exit(-1)
+    export = { k : str(v) for k,v in satinstance.features_dict.items() }
+    print(json.dumps(export, indent=None))
